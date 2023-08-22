5f63153958b72af05d9b3ec38888e20cbde2ff32
Gregory Edison <gregory.edison1993@gmail.com>
2023-08-22 18:21:23 +0200

    add katana logging

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -1,25 +1,32 @@
 #![cfg(feature = "ef-tests")]
 
 use ef_testing::models::suite::BlockchainTestSuite;
-use ef_tests::Suite;
-use rstest::rstest;
+use ef_testing::traits::Suite;
 
 macro_rules! blockchain_tests {
     ($test_name:ident, $dir:ident) => {
         #[tokio::test]
         async fn $test_name() {
-            BlockchainTestSuite::new(
-                format!("GeneralStateTests/{}", stringify!($dir)),
-                kakarot_test_env_ctx,
-            )
-            .run()
-            .await;
+            BlockchainTestSuite::new(format!("GeneralStateTests/{}", stringify!($dir)))
+                .run()
+                .await;
         }
     };
 }
 
 mod blockchain_tests {
     use super::*;
+    use ctor::ctor;
+    use tracing_subscriber::FmtSubscriber;
+
+    #[ctor]
+    fn setup() {
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(tracing::Level::INFO)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting tracing default failed");
+    }
 
     blockchain_tests!(vm_tests, VMTests);
 }
