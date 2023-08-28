c295d34b008ae0f190ba8163874bf5f4025ed6c6
ftupas <35031356+ftupas@users.noreply.github.com>
2023-08-28 13:05:06 +0200

    dev: fix deserialization error (#25)
    * chore: ignore katana dump
    
    * dev: fix deserialize for empty to address
    
    * test: add unit test

--- crates/ef-testing/src/models/mod.rs
+++ crates/ef-testing/src/models/mod.rs
@@ -2,13 +2,26 @@ pub mod case;
 pub mod result;
 pub mod suite;
 
+use std::str::FromStr;
+
 use reth_primitives::{Bytes, U64};
 use revm_primitives::{B160, B256};
-use serde::{self, Deserialize};
+use serde::{self, de, Deserialize, Deserializer};
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct BlockchainTestTransaction {
     pub transaction: Transaction,
+}
+
+fn deserialize_b160_optional<'de, D>(deserializer: D) -> Result<Option<B160>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    match s.as_str() {
+        "" => Ok(None),
+        _ => B160::from_str(&s).map(Some).map_err(de::Error::custom), // Convert string to B160 or return an error
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
@@ -19,7 +32,35 @@ pub struct Transaction {
     pub gas_price: U64,
     pub nonce: U64,
     pub secret_key: B256,
-    pub to: B160,
+    #[serde(deserialize_with = "deserialize_b160_optional")]
+    pub to: Option<B160>,
     pub sender: B160,
     pub value: Vec<U64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transaction_deserialization() {
+        let data = r#"
+        {
+            "data": ["0x1234"],
+            "gasLimit": ["0x1"],
+            "gasPrice": "0x1",
+            "nonce": "0x00",
+            "secretKey": "0x0000000000000000000000000000000000000000000000000000000000000123",
+            "sender": "0x00000000000000000000000000000000deadbeef",
+            "to": "",
+            "value": ["0x01"]
+        }
+        "#;
+
+        // Attempt to deserialize the JSON data
+        let result: Result<Transaction, _> = serde_json::from_str(data);
+
+        // Check if deserialization was successful
+        assert!(result.is_ok(), "Failed to deserialize: {:?}", result.err());
+    }
 }
