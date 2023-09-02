0b2af75ae719824f0a355288059f3ad3f3995beb
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-02 16:26:15 +0200

    introduce custom error type

---
+++ crates/ef-testing/src/models/error.rs
@@ -0,0 +1,81 @@
+use std::{fmt::Debug, path::PathBuf};
+
+use kakarot_rpc_core::{client::errors::EthApiError, models::ConversionError};
+use starknet::{
+    core::{types::FromByteArrayError, utils::NonAsciiNameError},
+    providers::ProviderError,
+};
+use starknet_api::StarknetApiError;
+
+/// Error type based off https://github.com/paradigmxyz/reth/blob/main/testing/ef-tests/src/result.rs
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum RunnerError {
+    /// Assertion error
+    #[error("{0}")]
+    Assertion(String),
+    /// An error occurred while decoding RLP.
+    #[error("An error occurred deserializing RLP")]
+    RlpDecodeError(#[from] reth_rlp::DecodeError),
+    /// An IO error occurred
+    #[error("An error occurred interacting with the file system at {path}: {error}")]
+    Io {
+        /// The path to the file or directory
+        path: PathBuf,
+        /// The specific error
+        error: String,
+    },
+    /// Skipped test
+    #[error("test skipped")]
+    Skipped,
+    /// Other
+    #[error("{0}")]
+    Other(String),
+}
+
+impl From<eyre::Error> for RunnerError {
+    fn from(err: eyre::Error) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl<E: std::error::Error> From<EthApiError<E>> for RunnerError {
+    fn from(err: EthApiError<E>) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl<E: std::error::Error> From<ProviderError<E>> for RunnerError {
+    fn from(err: ProviderError<E>) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl From<regex::Error> for RunnerError {
+    fn from(err: regex::Error) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl From<ConversionError<()>> for RunnerError {
+    fn from(err: ConversionError<()>) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl From<FromByteArrayError> for RunnerError {
+    fn from(err: FromByteArrayError) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl From<StarknetApiError> for RunnerError {
+    fn from(err: StarknetApiError) -> Self {
+        Self::Other(err.to_string())
+    }
+}
+
+impl From<NonAsciiNameError> for RunnerError {
+    fn from(err: NonAsciiNameError) -> Self {
+        Self::Other(err.to_string())
+    }
+}
