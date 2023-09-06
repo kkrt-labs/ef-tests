c3c783ee035a72614ea573169c712e1b88ccbd47
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-06 11:02:05 +0200

    refactor test setup

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -2,44 +2,16 @@
 
 use ef_testing::models::suite::BlockchainTestSuite;
 use ef_testing::traits::Suite;
+use std::sync::Once;
+use std::{format, fs};
+use tracing_subscriber::{filter, FmtSubscriber};
 
-macro_rules! blockchain_tests {
-    ($test_name:ident, $dir:ident) => {
-        #[tokio::test]
-        async fn $test_name() {
-            BlockchainTestSuite::new(format!("GeneralStateTests/{}", stringify!($dir)))
-                .run()
-                .await;
-        }
-    };
-}
+static INIT: Once = Once::new();
 
-mod blockchain_tests {
-
-    use super::*;
-    use ctor::ctor;
-    use std::{format, fs};
-    use tracing_subscriber::{filter, FmtSubscriber};
-
-    pub fn verify_kakarot_sha() -> Result<String, eyre::Error> {
-        let remote_sha = fs::read_to_string("../../.katana/remote_kakarot_sha")?;
-        let local_sha = fs::read_to_string("../../.katana/kakarot_sha")?;
-
-        if remote_sha != local_sha {
-            return Err(eyre::eyre!(format!(
-                "Kakarot commit hash mismatch: local: {}, remote: {}",
-                local_sha, remote_sha
-            )));
-        }
-
-        Ok(remote_sha)
-    }
-
-    #[ctor]
-    fn setup() {
+fn set_up() {
+    INIT.call_once(|| {
         // Set-up tracing filter
-        let filter =
-            filter::EnvFilter::new("tests::blockchain_tests=info,ef_testing=info,katana_core=info");
+        let filter = filter::EnvFilter::new("tests=info,ef_testing=info,katana_core=info");
         let subscriber = FmtSubscriber::builder().with_env_filter(filter).finish();
         tracing::subscriber::set_global_default(subscriber)
             .expect("setting tracing default failed");
@@ -55,7 +27,37 @@ mod blockchain_tests {
                 );
             }
         };
+    })
+}
+
+pub fn verify_kakarot_sha() -> Result<String, eyre::Error> {
+    let remote_sha = fs::read_to_string("../../.katana/remote_kakarot_sha")?;
+    let local_sha = fs::read_to_string("../../.katana/kakarot_sha")?;
+
+    if remote_sha != local_sha {
+        return Err(eyre::eyre!(format!(
+            "Kakarot commit hash mismatch: local: {}, remote: {}",
+            local_sha, remote_sha
+        )));
     }
+
+    Ok(remote_sha)
+}
+
+macro_rules! blockchain_tests {
+    ($test_name:ident, $dir:ident) => {
+        #[tokio::test]
+        async fn $test_name() {
+            set_up();
+            BlockchainTestSuite::new(format!("GeneralStateTests/{}", stringify!($dir)))
+                .run()
+                .await;
+        }
+    };
+}
+
+mod blockchain_tests {
+    use super::*;
 
     blockchain_tests!(vm_tests, VMTests);
 }
