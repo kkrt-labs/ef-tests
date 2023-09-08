bd10cc85bd3a6e102820dd06e02ba02d4f83162b
Elias Tazartes <elias.tazartes@gmail.com>
2023-09-08 17:33:57 +0900

    refactor: slight refactor

--- crates/ef-testing/src/bin/fetch_kakarot_submodule_commit.rs
+++ crates/ef-testing/src/bin/fetch_kakarot_submodule_commit.rs
@@ -1,5 +1,9 @@
+use reqwest::blocking::Client;
 use serde::Deserialize;
 
+/// Fetches the commit hash of the Kakarot submodule used inside the Kakarot-RPC repository
+/// Note that the latest commit of the Kakarot repository https://github.com/kkrt-labs/kakarot
+/// May not be aligned with the latest commit of https://github.com/kkrt-labs/kakarot-rpc/tree/main/lib/kakarot
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[derive(Deserialize)]
     struct Blob {
@@ -7,16 +11,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         sha: String,
     }
 
-    // Fetch Kakarot RPC tree
-    let url = "https://api.github.com/repos/sayajin-labs/kakarot-rpc/git/trees/main?recursive=1";
+    let kakarot_rpc_tree_url =
+        "https://api.github.com/repos/kkrt-labs/kakarot-rpc/git/trees/main?recursive=1";
 
-    let client = reqwest::blocking::Client::builder();
-    let response: serde_json::Value = client
-        .user_agent("reqwest-rust")
-        .build()?
-        .get(url)
-        .send()?
-        .json()?;
+    let client = Client::builder().user_agent("reqwest-rust").build()?;
+    let response: serde_json::Value = client.get(kakarot_rpc_tree_url).send()?.json()?;
 
     // Filter the blobs to only include kakarot submodule
     let blobs: Vec<Blob> = serde_json::from_value(response["tree"].clone())?;
