6bc075deeb0a51ac36fa040f9a4ab2937eb94727
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-19 14:05:07 +0700

    add rust doc

--- crates/sequencer/src/state.rs
+++ crates/sequencer/src/state.rs
@@ -9,6 +9,11 @@ use starknet_api::{
 
 #[derive(Default)]
 /// Generic state structure for the sequencer.
+/// The use of FxHashMap allows for a better performance.
+/// This hash map is used by rustc. It uses a non cryptographic hash function
+/// which is faster than the default hash function. Think about changing
+/// if the test sequencer is used for tests outside of ef-tests.
+/// See [rustc-hash](https://crates.io/crates/rustc-hash) for more information.
 pub struct State {
     classes: FxHashMap<ClassHash, ContractClass>,
     contracts: FxHashMap<ContractAddress, ClassHash>,
