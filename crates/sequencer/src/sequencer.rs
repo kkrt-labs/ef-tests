e072e541760e9160fd2d159f7f514b205ed6914b
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-19 15:10:43 +0700

    comments + linting

--- crates/sequencer/src/sequencer.rs
+++ crates/sequencer/src/sequencer.rs
@@ -5,8 +5,8 @@ use blockifier::state::state_api::{State, StateReader};
 /// Using a trait bound for the state allows for better
 /// speed, as the type of the state is known at compile time.
 pub struct Sequencer<S: State + StateReader> {
-    config: SequencerConfig,
-    state: S,
+    pub config: SequencerConfig,
+    pub state: S,
 }
 
 impl<S: State + StateReader> Sequencer<S> {
--- crates/sequencer/src/state.rs
+++ crates/sequencer/src/state.rs
@@ -2,21 +2,23 @@ use blockifier::{
     execution::contract_class::ContractClass, state::cached_state::ContractStorageKey,
 };
 use rustc_hash::FxHashMap;
+use starknet_api::core::CompiledClassHash;
 use starknet_api::{
     core::{ClassHash, ContractAddress, Nonce},
     hash::StarkFelt,
 };
 
-#[derive(Default)]
 /// Generic state structure for the sequencer.
 /// The use of FxHashMap allows for a better performance.
 /// This hash map is used by rustc. It uses a non cryptographic hash function
 /// which is faster than the default hash function. Think about changing
 /// if the test sequencer is used for tests outside of ef-tests.
 /// See [rustc-hash](https://crates.io/crates/rustc-hash) for more information.
+#[derive(Default)]
 pub struct State {
-    classes: FxHashMap<ClassHash, ContractClass>,
-    contracts: FxHashMap<ContractAddress, ClassHash>,
-    storage: FxHashMap<ContractStorageKey, StarkFelt>,
-    nonces: FxHashMap<ContractAddress, Nonce>,
+    pub classes: FxHashMap<ClassHash, ContractClass>,
+    pub compiled_classes: FxHashMap<ClassHash, CompiledClassHash>,
+    pub contracts: FxHashMap<ContractAddress, ClassHash>,
+    pub storage: FxHashMap<ContractStorageKey, StarkFelt>,
+    pub nonces: FxHashMap<ContractAddress, Nonce>,
 }
