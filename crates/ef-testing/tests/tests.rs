e949a8493e8bcf3473aa5d6d922716fec0d7315a
Khaeljy <contact@khaeljy.fr>
2023-09-25 12:46:23 +0200

    add `stBugs` testsuite to the blockchain_tests module (#261)
    Co-authored-by: Elias Tazartes <66871571+Eikix@users.noreply.github.com>

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -70,6 +70,7 @@ mod blockchain_tests {
     blockchain_tests!(st_log_tests, stLogTests);
     blockchain_tests!(st_bad_opcode, stBadOpcode);
     blockchain_tests!(shanghai, Shanghai);
+    blockchain_tests!(st_bugs, stBugs);
     blockchain_tests!(st_call_codes, stCallCodes);
     //blockchain_tests!(st_call_create_call_code_test, stCallCreateCallCodeTest); // ef-tests #257
     blockchain_tests!(
