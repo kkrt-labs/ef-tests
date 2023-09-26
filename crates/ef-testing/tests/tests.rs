cb728fe40c9e46da160f900a16ed0025953f9192
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2023-09-26 16:25:13 +0700

    organize tests in folder order

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -62,20 +62,13 @@ macro_rules! blockchain_tests {
 mod blockchain_tests {
     use super::*;
 
-    blockchain_tests!(st_sload, stSLoadTest);
-    blockchain_tests!(vm_tests, VMTests);
-    blockchain_tests!(memory_tests, stMemoryTest);
-    blockchain_tests!(init_code_test, stInitCodeTest);
-    blockchain_tests!(st_solidity_test, stSolidityTest);
-    blockchain_tests!(st_log_tests, stLogTests);
-    blockchain_tests!(st_bad_opcode, stBadOpcode);
     blockchain_tests!(shanghai, Shanghai);
-    blockchain_tests!(st_code_copy_test, stCodeCopyTest);
-    blockchain_tests!(st_code_size_limit, stCodeSizeLimit);
+    // blockchain_tests!(st_args_zero_one_balance, stArgsZeroOneBalance); // TODO: Implement tests
+    // blockchain_tests!(st_attack_test, stAttackTest); // TODO: Implement tests
+    blockchain_tests!(st_bad_opcode, stBadOpcode);
     blockchain_tests!(st_bugs, stBugs);
     blockchain_tests!(st_call_codes, stCallCodes);
-    blockchain_tests!(st_ext_code_hash, stExtCodeHash);
-    //blockchain_tests!(st_call_create_call_code_test, stCallCreateCallCodeTest); // ef-tests #257
+    // blockchain_tests!(st_call_create_call_code_test, stCallCreateCallCodeTest); // ef-tests #257
     blockchain_tests!(
         st_call_delegate_codes_call_code_homestead,
         stCallDelegateCodesCallCodeHomestead
@@ -84,5 +77,53 @@ mod blockchain_tests {
         st_call_delegate_codes_homestead,
         stCallDelegateCodesHomestead
     );
+    // blockchain_tests!(st_chain_id, stChainId); // TODO: Implement tests
+    // blockchain_tests!(st_code_copy_test, stCodeCopyTest); // TODO: Implement tests
+    // blockchain_tests!(st_code_size_limit, stCodeSizeLimit); // TODO: Implement tests
+    // blockchain_tests!(st_create2, stCreate2); // TODO: Implement tests
+    // blockchain_tests!(st_create_test, stCreateTest); // TODO: Implement tests
     blockchain_tests!(st_delegatecall_test_homestead, stDelegatecallTestHomestead);
+    // blockchain_tests!(st_eip150_specific, stEIP150Specific); // TODO: Implement tests
+    // blockchain_tests!(st_eip150single_code_gas_prices, stEIP150singleCodeGasPrices); // TODO: Implement tests
+    // blockchain_tests!(st_eip1559, stEIP1559); // TODO: Implement tests
+    // blockchain_tests!(st_eip158_specific, stEIP158Specific); // TODO: Implement tests
+    // blockchain_tests!(st_eip2930, stEIP2930); // TODO: Implement tests
+    // blockchain_tests!(st_eip3607, stEIP3607); // TODO: Implement tests
+    // blockchain_tests!(st_example, stExample); // TODO: Implement tests
+    blockchain_tests!(st_ext_code_hash, stExtCodeHash);
+    // blockchain_tests!(st_homestead_specific, stHomesteadSpecific); // TODO: Implement tests
+    blockchain_tests!(st_init_code_test, stInitCodeTest);
+    blockchain_tests!(st_log_tests, stLogTests);
+    // blockchain_tests!(st_mem_expanding_eip150_calls, stMemExpandingEIP150Calls); // TODO: Implement tests
+    // blockchain_tests!(st_memory_stress_test, stMemoryStressTest); // TODO: Implement tests
+    blockchain_tests!(st_memory_test, stMemoryTest);
+    // blockchain_tests!(st_non_zero_calls_test, stNonZeroCallsTest); // TODO: Implement tests
+    // blockchain_tests!(st_pre_compiled_contracts, stPreCompiledContracts); // TODO: Implement tests
+    // blockchain_tests!(st_pre_compiled_contracts2, stPreCompiledContracts2); // TODO: Implement tests
+    // blockchain_tests!(st_quadratic_complexity_test, stQuadraticComplexityTest); // TODO: Implement tests
+    // blockchain_tests!(st_random, stRandom); // TODO: Implement tests
+    // blockchain_tests!(st_random2, stRandom2); // TODO: Implement tests
+    // blockchain_tests!(st_recursive_create, stRecursiveCreate); // TODO: Implement tests
+    // blockchain_tests!(st_refund_test, stRefundTest); // TODO: Implement tests
+    // blockchain_tests!(st_return_data_test, stReturnDataTest); // TODO: Implement tests
+    // blockchain_tests!(st_revert_test, stRevertTest); // TODO: Implement tests
+    blockchain_tests!(st_sload_test, stSLoadTest);
+    // blockchain_tests!(st_sstore_test, stSStoreTest); // TODO: Implement tests
+    // blockchain_tests!(st_self_balance, stSelfBalance); // TODO: Implement tests
+    // blockchain_tests!(st_shift, stShift); // TODO: Implement tests
+    blockchain_tests!(st_solidity_test, stSolidityTest);
+    // blockchain_tests!(st_special_test, stSpecialTest); // TODO: Implement tests
+    // blockchain_tests!(st_stack_tests, stStackTests); // TODO: Implement tests
+    // blockchain_tests!(st_static_call, stStaticCall); // TODO: Implement tests
+    // blockchain_tests!(st_static_flag_enabled, stStaticFlagEnabled); // TODO: Implement tests
+    // blockchain_tests!(st_system_operations_test, stSystemOperationsTest); // TODO: Implement tests
+    // blockchain_tests!(st_time_consuming, stTimeConsuming); // TODO: Implement tests
+    // blockchain_tests!(st_transaction_test, stTransactionTest); // TODO: Implement tests
+    // blockchain_tests!(st_transition_test, stTransitionTest); // TODO: Implement tests
+    // blockchain_tests!(st_wallet_test, stWalletTest); // TODO: Implement tests
+    // blockchain_tests!(st_zero_calls_revert, stZeroCallsRevert); // TODO: Implement tests
+    // blockchain_tests!(st_zero_calls_test, stZeroCallsTest); // TODO: Implement tests
+    // blockchain_tests!(st_zero_knowledge, stZeroKnowledge); // TODO: Implement tests
+    // blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2); // TODO: Implement tests
+    blockchain_tests!(vm_tests, VmTests);
 }
