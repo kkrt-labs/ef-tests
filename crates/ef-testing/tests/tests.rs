2b13668f4722a6f4c545be50562936d85b872a9c
danilowhk <danilowhk@gmail.com>
2023-09-28 14:46:36 +0700

    Added stReturnDataTest (#321)
    * add numbers of failing tests
    
    * use normal comments
    
    * add remaning failing tests numbers
    
    * st_return_data_test added
    
    ---------
    
    Co-authored-by: Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -62,8 +62,13 @@ macro_rules! blockchain_tests {
 mod blockchain_tests {
     use super::*;
 
+    // TODO: Uncomment tests to add entire folder to test flow.
+    // All failing tests should be added to the skip session at "blockchain-tests-skip.yml"
+    // and a respective issue at https://github.com/kkrt-labs/ef-tests/issues should be created.
+    // Tests from the same folder with the same error can be aggregated in one issue
+
     blockchain_tests!(shanghai, Shanghai);
-    // blockchain_tests!(st_args_zero_one_balance, stArgsZeroOneBalance); // TODO: Implement tests
+    // blockchain_tests!(st_args_zero_one_balance, stArgsZeroOneBalance); // 4 failing tests
     blockchain_tests!(st_attack_test, stAttackTest);
     blockchain_tests!(st_bad_opcode, stBadOpcode);
     blockchain_tests!(st_bugs, stBugs);
@@ -80,50 +85,50 @@ mod blockchain_tests {
     blockchain_tests!(st_chain_id, stChainId);
     blockchain_tests!(st_code_copy_test, stCodeCopyTest);
     blockchain_tests!(st_code_size_limit, stCodeSizeLimit);
-    // blockchain_tests!(st_create2, stCreate2); // TODO: Implement tests
-    // blockchain_tests!(st_create_test, stCreateTest); // TODO: Implement tests
+    // blockchain_tests!(st_create2, stCreate2); // 42 failing tests
+    // blockchain_tests!(st_create_test, stCreateTest); // 34 failing tests
     blockchain_tests!(st_delegatecall_test_homestead, stDelegatecallTestHomestead);
     blockchain_tests!(st_eip150_specific, stEIP150Specific);
-    // blockchain_tests!(st_eip150single_code_gas_prices, stEIP150singleCodeGasPrices); // TODO: Implement tests
-    // blockchain_tests!(st_eip1559, stEIP1559); // TODO: Implement tests
-    // blockchain_tests!(st_eip158_specific, stEIP158Specific); // TODO: Implement tests
-    // blockchain_tests!(st_eip2930, stEIP2930); // TODO: Implement tests
-    // blockchain_tests!(st_eip3607, stEIP3607); // TODO: Implement tests
-    // blockchain_tests!(st_example, stExample); // TODO: Implement tests
+    // blockchain_tests!(st_eip150single_code_gas_prices, stEIP150singleCodeGasPrices); // 34 failing tests
+    // blockchain_tests!(st_eip1559, stEIP1559); // failing due to missing field gasPrice, cannot measure number of failing tests
+    // blockchain_tests!(st_eip158_specific, stEIP158Specific); // 6 failing tests
+    // blockchain_tests!(st_eip2930, stEIP2930); // failing due to missing field gasPrice, cannot measure number of failing tests
+    blockchain_tests!(st_eip3607, stEIP3607);
+    // blockchain_tests!(st_example, stExample); // failing due to missing field gasPrice, cannot measure number of failing tests
     blockchain_tests!(st_ext_code_hash, stExtCodeHash);
-    // blockchain_tests!(st_homestead_specific, stHomesteadSpecific); // TODO: Implement tests
+    // blockchain_tests!(st_homestead_specific, stHomesteadSpecific); // 4 failing tests
     blockchain_tests!(st_init_code_test, stInitCodeTest);
     blockchain_tests!(st_log_tests, stLogTests);
-    // blockchain_tests!(st_mem_expanding_eip150_calls, stMemExpandingEIP150Calls); // TODO: Implement tests
-    // blockchain_tests!(st_memory_stress_test, stMemoryStressTest); // TODO: Implement tests
+    // blockchain_tests!(st_mem_expanding_eip150_calls, stMemExpandingEIP150Calls); // 7 failing tests
+    // blockchain_tests!(st_memory_stress_test, stMemoryStressTest); // 2 failing tests
     blockchain_tests!(st_memory_test, stMemoryTest);
-    // blockchain_tests!(st_non_zero_calls_test, stNonZeroCallsTest); // TODO: Implement tests
-    // blockchain_tests!(st_pre_compiled_contracts, stPreCompiledContracts); // TODO: Implement tests
-    // blockchain_tests!(st_pre_compiled_contracts2, stPreCompiledContracts2); // TODO: Implement tests
-    // blockchain_tests!(st_quadratic_complexity_test, stQuadraticComplexityTest); // TODO: Implement tests
-    // blockchain_tests!(st_random, stRandom); // TODO: Implement tests
-    // blockchain_tests!(st_random2, stRandom2); // TODO: Implement tests
-    // blockchain_tests!(st_recursive_create, stRecursiveCreate); // TODO: Implement tests
-    // blockchain_tests!(st_refund_test, stRefundTest); // TODO: Implement tests
-    // blockchain_tests!(st_return_data_test, stReturnDataTest); // TODO: Implement tests
-    // blockchain_tests!(st_revert_test, stRevertTest); // TODO: Implement tests
+    // blockchain_tests!(st_non_zero_calls_test, stNonZeroCallsTest); // 13 failing tests
+    // blockchain_tests!(st_pre_compiled_contracts, stPreCompiledContracts); // 8 failing tests
+    // blockchain_tests!(st_pre_compiled_contracts2, stPreCompiledContracts2); // 97 failing tests
+    // blockchain_tests!(st_quadratic_complexity_test, stQuadraticComplexityTest); // 7 failing tests
+    // blockchain_tests!(st_random, stRandom); // 79 failing tests
+    // blockchain_tests!(st_random2, stRandom2); // 40 failing tests
+    // blockchain_tests!(st_recursive_create, stRecursiveCreate); // 2 failing tests
+    // blockchain_tests!(st_refund_test, stRefundTest); // 9 failing tests
+    blockchain_tests!(st_return_data_test, stReturnDataTest);
+    // blockchain_tests!(st_revert_test, stRevertTest); // 23 failing tests
     blockchain_tests!(st_sload_test, stSLoadTest);
-    // blockchain_tests!(st_sstore_test, stSStoreTest); // TODO: Implement tests
-    // blockchain_tests!(st_self_balance, stSelfBalance); // TODO: Implement tests
-    // blockchain_tests!(st_shift, stShift); // TODO: Implement tests
+    // blockchain_tests!(st_sstore_test, stSStoreTest); // 27 failing tests
+    // blockchain_tests!(st_self_balance, stSelfBalance); // 3 failing tests
+    // blockchain_tests!(st_shift, stShift); // 2 failing tests
     blockchain_tests!(st_solidity_test, stSolidityTest);
-    // blockchain_tests!(st_special_test, stSpecialTest); // TODO: Implement tests
-    // blockchain_tests!(st_stack_tests, stStackTests); // TODO: Implement tests
-    // blockchain_tests!(st_static_call, stStaticCall); // TODO: Implement tests
-    // blockchain_tests!(st_static_flag_enabled, stStaticFlagEnabled); // TODO: Implement tests
-    // blockchain_tests!(st_system_operations_test, stSystemOperationsTest); // TODO: Implement tests
-    // blockchain_tests!(st_time_consuming, stTimeConsuming); // TODO: Implement tests
-    // blockchain_tests!(st_transaction_test, stTransactionTest); // TODO: Implement tests
-    // blockchain_tests!(st_transition_test, stTransitionTest); // TODO: Implement tests
-    // blockchain_tests!(st_wallet_test, stWalletTest); // TODO: Implement tests
-    // blockchain_tests!(st_zero_calls_revert, stZeroCallsRevert); // TODO: Implement tests
-    // blockchain_tests!(st_zero_calls_test, stZeroCallsTest); // TODO: Implement tests
-    // blockchain_tests!(st_zero_knowledge, stZeroKnowledge); // TODO: Implement tests
-    // blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2); // TODO: Implement tests
+    // blockchain_tests!(st_special_test, stSpecialTest); // 3 failing tests
+    // blockchain_tests!(st_stack_tests, stStackTests); // 7 failing tests
+    // blockchain_tests!(st_static_call, stStaticCall); // 207 failing tests
+    // blockchain_tests!(st_static_flag_enabled, stStaticFlagEnabled); // 13 failing tests
+    // blockchain_tests!(st_system_operations_test, stSystemOperationsTest); // 28 failing tests
+    // blockchain_tests!(st_time_consuming, stTimeConsuming); // 14 failing tests
+    // blockchain_tests!(st_transaction_test, stTransactionTest); // failing due to: invalid length 62, expected a (both 0x-prefixed or not) hex string or byte array containing betwee
+    // blockchain_tests!(st_transition_test, stTransitionTest); // 3 failing tests
+    // blockchain_tests!(st_wallet_test, stWalletTest); // 5 failing tests
+    blockchain_tests!(st_zero_calls_revert, stZeroCallsRevert);
+    // blockchain_tests!(st_zero_calls_test, stZeroCallsTest); // 1 failing test
+    // blockchain_tests!(st_zero_knowledge, stZeroKnowledge); // 60 failing
+    // blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2); // 59 failing
     blockchain_tests!(vm_tests, VmTests);
 }
