31fb31fe1b108340ce65c1ffa15e9cd6885cbc3a
TAdev0 <angles.tristan@protonmail.com>
2023-09-30 15:17:50 +0200

    stSystemOperationsTest

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -121,7 +121,7 @@ mod blockchain_tests {
     // blockchain_tests!(st_stack_tests, stStackTests); // 7 failing tests
     // blockchain_tests!(st_static_call, stStaticCall); // 207 failing tests
     // blockchain_tests!(st_static_flag_enabled, stStaticFlagEnabled); // 13 failing tests
-    // blockchain_tests!(st_system_operations_test, stSystemOperationsTest); // 28 failing tests
+    blockchain_tests!(st_system_operations_test, stSystemOperationsTest);
     // blockchain_tests!(st_time_consuming, stTimeConsuming); // 14 failing tests
     // blockchain_tests!(st_transaction_test, stTransactionTest); // failing due to: invalid length 62, expected a (both 0x-prefixed or not) hex string or byte array containing betwee
     // blockchain_tests!(st_transition_test, stTransitionTest); // 3 failing tests
