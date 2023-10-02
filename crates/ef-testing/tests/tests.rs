420bb31fe305f6dfba823bdaf4aea792c5a95dbd
TAdev0 <angles.tristan@protonmail.com>
2023-10-02 17:59:19 +0200

    stZeroKnowledge

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -138,7 +138,7 @@ mod blockchain_tests {
     blockchain_tests!(st_zero_knowledge, stZeroKnowledge);
     blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2);
     blockchain_tests!(st_zero_calls_test, stZeroCallsTest);
-    // blockchain_tests!(st_zero_knowledge, stZeroKnowledge); // 60 failing
-    // blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2); // 59 failing
+    blockchain_tests!(st_zero_knowledge, stZeroKnowledge);
+    blockchain_tests!(st_zero_knowledge2, stZeroKnowledge2);
     blockchain_tests!(vm_tests, VmTests);
 }
