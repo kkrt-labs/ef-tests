b9d014261850ea82a2ba488002eb9a54b3e3148b
Elias Tazartes <elias.tazartes@gmail.com>
2023-10-06 16:26:43 +0700

    fix: add storage checks for wiped storage slots

--- crates/ef-testing/src/utils/mod.rs
+++ crates/ef-testing/src/utils/mod.rs
@@ -2,7 +2,7 @@ use std::{collections::BTreeMap, fs, path::Path};
 
 use ef_tests::models::{Account, State};
 use reth_primitives::{keccak256, Address, Bytes, JsonU256};
-use revm_primitives::{B160, B256};
+use revm_primitives::{B160, B256, U256};
 use secp256k1::{PublicKey, SecretKey};
 use serde::Deserialize;
 
@@ -39,6 +39,31 @@ pub(crate) fn update_post_state(
     pre_state: State,
 ) -> BTreeMap<B160, Account> {
     for (k, _) in pre_state.iter() {
+        // If the post account's storage does not contain a key from the pre-state,
+        // It means its storage was deleted
+        // We need to insert in the tree that we use for assertions value 0x00 at this storage key.
+        if post_state.contains_key(k) {
+            let post_account_storage = &post_state
+                .get(k)
+                .map(|x| x.storage.clone())
+                .unwrap_or(BTreeMap::new());
+
+            let pre_account_storage = pre_state
+                .get(k)
+                .map(|x| x.storage.clone())
+                .unwrap_or(BTreeMap::new());
+
+            for storage_key in pre_account_storage.keys() {
+                if !post_account_storage.contains_key(storage_key) {
+                    post_state
+                        .get_mut(k)
+                        .unwrap()
+                        .storage
+                        .insert(*storage_key, U256::ZERO.into());
+                }
+            }
+        }
+
         if !post_state.contains_key(k) {
             post_state.insert(
                 *k,
