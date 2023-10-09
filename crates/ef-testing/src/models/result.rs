b82d3c8ae1431fd1683df85de5d15bcd7dcc97fc
greged93 <82421016+greged93@users.noreply.github.com>
2023-10-09 19:22:23 +0200

    fix logging (#511)

--- crates/ef-testing/src/models/result.rs
+++ crates/ef-testing/src/models/result.rs
@@ -29,26 +29,40 @@ impl CaseResult {
 }
 
 /// Assert that all the given tests passed and print the results to stdout.
-pub(crate) fn assert_tests_pass(_suite_name: &str, _path: &Path, results: &[CaseResult]) {
-    let (_passed, failed, _skipped) = categorize_results(results);
+pub(crate) fn assert_tests_pass(suite_name: &str, results: &[CaseResult]) {
+    let failed = categorize_results(results);
+
+    print_results(suite_name, &failed);
+
     assert!(failed.is_empty(), "Some tests failed (see above)");
 }
 
 /// Categorize test results into `(passed, failed, skipped)`.
-pub(crate) fn categorize_results(
-    results: &[CaseResult],
-) -> (Vec<&CaseResult>, Vec<&CaseResult>, Vec<&CaseResult>) {
-    let mut passed = Vec::new();
+pub(crate) fn categorize_results(results: &[CaseResult]) -> Vec<&CaseResult> {
     let mut failed = Vec::new();
-    let mut skipped = Vec::new();
 
     for case in results {
         match case.result.as_ref().err() {
-            Some(RunnerError::Skipped) => skipped.push(case),
+            Some(RunnerError::Skipped) => (),
             Some(_) => failed.push(case),
-            None => passed.push(case),
+            None => (),
         }
     }
 
-    (passed, failed, skipped)
+    failed
+}
+
+/// Display the given test results to stdout.
+pub(crate) fn print_results(suite_name: &str, failed: &[&CaseResult]) {
+    for case in failed {
+        match &case.result {
+            Ok(_) => unreachable!(),
+            Err(err) => println!(
+                "[!] Suite {} Case {} failed:\n{}",
+                suite_name,
+                case.path.display(),
+                err,
+            ),
+        }
+    }
 }
