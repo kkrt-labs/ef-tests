735637d449baeb249eead37297e2495e74d9afc4
Gregory Edison <gregory.edison1993@gmail.com>
2023-10-17 12:17:57 +0200

    fix converter

--- crates/ef-testing-proc-macro/src/constants.rs
+++ crates/ef-testing-proc-macro/src/constants.rs
@@ -1,9 +1,9 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 lazy_static::lazy_static! {
-    pub(crate) static ref TEST_FILTER_PATH: &'static Path =  Path::new("blockchain-tests-skip.yml");
+    pub(crate) static ref TEST_FILTER_PATH: PathBuf =  Path::new("blockchain-tests-skip.yml").to_path_buf();
+    pub(crate) static ref REGEX: regex::Regex = regex::Regex::new(r"[=^.]").unwrap();
 }
 
-pub(crate) const REGEX_STR: &str = r"[=^.]";
 pub(crate) const ROOT: &str = "GeneralStateTests";
 pub(crate) const FORK: &str = "Shanghai";
--- crates/ef-testing-proc-macro/src/converter.rs
+++ crates/ef-testing-proc-macro/src/converter.rs
@@ -1,18 +1,16 @@
 use std::collections::BTreeMap;
 
-use ef_tests::models::BlockchainTest;
 use rayon::prelude::*;
-use regex::Regex;
+use serde_json::Value;
 
 use crate::{
-    constants::{FORK, REGEX_STR},
+    constants::{FORK, REGEX},
+    content_reader::ContentReader,
     dir_reader::{DirReader, PathWrapper},
 };
 
 /// The TestConverter is used to convert all the
 /// tests files in to rust tests.
-/// Filter is an optional filter to be applied on
-/// the test file's name.
 pub struct TestConverter {
     directory: DirReader,
 }
@@ -22,6 +20,8 @@ impl TestConverter {
         Self { directory }
     }
 
+    /// Converts the given directory into a String containing all
+    /// the rust tests to be ran.
     pub fn convert(&self) -> Result<String, eyre::Error> {
         self.directory
             .sub_dirs
@@ -31,6 +31,7 @@ impl TestConverter {
                 |acc, (folder_name, node)| {
                     let mut s = String::new();
                     s += &Self::format_to_module(folder_name);
+                    s += &Self::secret_key(node)?;
                     s += &Self::convert_folders(node)?;
                     s += "}";
                     Ok(acc? + &s)
@@ -39,57 +40,105 @@ impl TestConverter {
             .collect()
     }
 
+    /// Converts the given directory into a String.
     fn convert_folders(node: &DirReader) -> Result<String, eyre::Error> {
         let mut acc = String::new();
         for (dir_name, sub_node) in &node.sub_dirs {
             acc += &TestConverter::format_to_module(dir_name);
+            acc += &Self::secret_key(sub_node)?;
             acc += &Self::convert_folders(sub_node)?;
             acc += "}";
         }
         Ok(acc + &Self::convert_files(&node.files)?)
     }
 
+    #[allow(clippy::manual_try_fold)]
+    /// Converts the given files into a String.
     fn convert_files(files: &[(PathWrapper, bool)]) -> Result<String, eyre::Error> {
         let mut acc = String::new();
         for (file_path, is_skipped) in files {
             let content = file_path.read_file_to_string()?;
-            // let general_state_tests_path = path
-            // .components()
-            // .filter(|x| !x.as_os_str().eq_ignore_ascii_case("BlockchainTests"))
-            // .collect::<PathBuf>();
             let cases: BTreeMap<String, serde_json::Value> = serde_json::from_str(&content)?;
-            acc += &cases.into_keys().fold(String::new(), |acc, case_name| {
-                if !case_name.contains(FORK) {
-                    return acc;
-                }
-                acc + &TestConverter::format_to_test(&case_name, *is_skipped)
-            })
+            acc += &cases.into_iter().fold(
+                Ok(String::new()),
+                |acc: Result<String, eyre::Error>, (case_name, content)| {
+                    if !case_name.contains(FORK) {
+                        return acc;
+                    }
+                    Ok(acc? + &TestConverter::format_to_test(&case_name, content, *is_skipped)?)
+                },
+            )?
         }
         Ok(acc)
     }
 
+    /// Formats the given folder name into a rust module.
     fn format_to_module(folder_name: &str) -> String {
         format!(
-            r#"mod {} {{"#,
+            r#"mod {} {{
+            use super::*;
+            "#,
             TestConverter::format_into_identifier(folder_name)
         )
     }
 
-    fn format_to_test(case_name: &str, is_skipped: bool) -> String {
-        format!(
+    /// Formats the secret key into a rust constant.
+    fn secret_key(node: &DirReader) -> Result<String, eyre::Error> {
+        if node.files.is_empty() {
+            return Ok(String::default());
+        }
+        let sk = ContentReader::secret_key(node.files.first().unwrap().0.clone())?;
+        Ok(format!(r#"pub const SK: &str = {};"#, &sk))
+    }
+
+    /// Formats the given test case into a rust test.
+    fn format_to_test(
+        case_name: &str,
+        content: Value,
+        is_skipped: bool,
+    ) -> Result<String, eyre::Error> {
+        Ok(format!(
             r#"
             #[tokio::test(flavor = "multi_thread")]
             {}
             async fn test_{}() {{
+                {}
             }}"#,
             if is_skipped { "#[ignore]" } else { "" },
             TestConverter::format_into_identifier(case_name),
-        )
+            Self::format_test_content(case_name, &content, is_skipped)?,
+        ))
     }
 
+    /// Formats the given test content into a rust test.
+    fn format_test_content(
+        case_name: &str,
+        content: &Value,
+        is_skipped: bool,
+    ) -> Result<String, eyre::Error> {
+        if is_skipped {
+            return Ok(String::default());
+        }
+        let block = ContentReader::block(content)?;
+        let pre = ContentReader::pre_state(content)?;
+        let post = ContentReader::post_state(content)?;
+        Ok(format!(
+            r##"
+            setup();
+            let block: Block = serde_json::from_str(r#"{}"#).expect("Error while reading the block");
+            let pre: State = serde_json::from_str(r#"{}"#).expect("Error while reading the pre state");
+            let post: RootOrState = serde_json::from_str(r#"{}"#).expect("Error while reading the post state");
+            let case = BlockchainTestCase::new("{}".to_string(), block, pre, post, B256::from_str(SK).expect("Error while reading  secret key"));
+            case.run().expect("Error while running the test");
+        "##,
+            block, pre, post, case_name
+        ))
+    }
+
+    /// Formats the given string into a valid rust identifier.
     fn format_into_identifier(s: &str) -> String {
-        let re = Regex::new(REGEX_STR).expect("Error while compiling the regex"); // safe unwrap
-        re.replace_all(s, "_")
+        REGEX
+            .replace_all(s, "_")
             .replace('-', "_minus_")
             .replace('+', "_plus_")
     }
