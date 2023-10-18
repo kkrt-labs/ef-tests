1cc9e7976ba23b0e6db3dfe38c146c41d242f986
Gregory Edison <gregory.edison1993@gmail.com>
2023-10-18 09:39:39 +0200

    rename to ef-testing-proc-macros

--- crates/ef-testing/tests/tests.rs
+++ crates/ef-testing/tests/tests.rs
@@ -2,7 +2,7 @@ use std::{str::FromStr, sync::Once};
 
 use ef_testing::models::case::BlockchainTestCase;
 use ef_testing::traits::Case;
-use ef_testing_proc_macro::generate_blockchain_tests;
+use ef_testing_proc_macros::generate_blockchain_tests;
 use ef_tests::models::{Block, RootOrState, State};
 use revm_primitives::B256;
 use tracing_subscriber::{filter, FmtSubscriber};
