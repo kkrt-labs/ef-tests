30db175049939f59ab8cb267c35acd490fe13606
Gregory Edison <gregory.edison1993@gmail.com>
2023-11-11 10:56:58 +0300

    remove the match

--- crates/ef-testing/src/lib.rs
+++ crates/ef-testing/src/lib.rs
@@ -7,9 +7,7 @@ pub mod utils;
 use bytes::BytesMut;
 use evm_sequencer::constants::CHAIN_ID;
 use models::error::RunnerError;
-use reth_primitives::{
-    sign_message, AccessList, Bytes, SealedBlock, Signature, Transaction, TxEip2930,
-};
+use reth_primitives::{sign_message, Bytes, SealedBlock, Signature, Transaction};
 use reth_rlp::Decodable;
 use revm_primitives::B256;
 
@@ -24,28 +22,15 @@ pub fn get_signed_rlp_encoded_transaction(block: &Bytes, pk: B256) -> Result<Byt
     let block = SealedBlock::decode(&mut block.as_ref()).map_err(RunnerError::RlpDecodeError)?;
 
     // Encode body as transaction
-    let mut out = BytesMut::new();
-    let tx_signed = block.body.first().cloned().ok_or_else(|| {
+    let mut tx_signed = block.body.first().cloned().ok_or_else(|| {
         RunnerError::Other(vec!["No transaction in pre state block".into()].into())
     })?;
 
-    let mut tx = match &tx_signed.transaction {
-        Transaction::Legacy(tx) => Transaction::Eip2930(TxEip2930 {
-            chain_id: *CHAIN_ID,
-            nonce: tx.nonce,
-            gas_limit: tx.gas_limit,
-            gas_price: tx.gas_price,
-            to: tx.to,
-            value: tx.value,
-            access_list: AccessList::default(),
-            input: tx.input.clone(),
-        }),
-        _ => tx_signed.transaction,
-    };
+    tx_signed.transaction.set_chain_id(*CHAIN_ID);
+    let signature = sign_tx(&tx_signed.transaction, &pk)?;
 
-    tx.set_chain_id(*CHAIN_ID);
-    let signature = sign_tx(&tx, &pk)?;
-    tx.encode_with_signature(&signature, &mut out, false);
+    let mut out = BytesMut::new();
+    tx_signed.encode_with_signature(&signature, &mut out, false);
 
     Ok(out.to_vec().into())
 }
