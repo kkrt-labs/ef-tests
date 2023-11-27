79e32df9a50b16edd7ff6422978b54cae4e3f315
Gregory Edison <gregory.edison1993@gmail.com>
2023-11-27 13:51:55 +0100

    update print of reversion reason

--- crates/ef-testing/src/models/result.rs
+++ crates/ef-testing/src/models/result.rs
@@ -1,5 +1,9 @@
-use blockifier::transaction::objects::{TransactionExecutionInfo, TransactionExecutionResult};
-use starknet::core::types::FieldElement;
+use blockifier::{
+    execution::call_info::CallInfo,
+    transaction::objects::{TransactionExecutionInfo, TransactionExecutionResult},
+};
+use starknet::{core::types::FieldElement, macros::selector};
+use starknet_api::{hash::StarkFelt, transaction::EventContent};
 use tracing::{error, info, warn};
 
 pub(crate) fn log_execution_result(
@@ -13,15 +17,25 @@ pub(crate) fn log_execution_result(
             } else {
                 info!("{} passed: {:?}", case_name, info.actual_resources);
                 if let Some(call) = info.execute_call_info {
-                    let revert_message: String = call
-                        .execution
-                        .retdata
-                        .0
-                        .into_iter()
-                        .filter_map(|d| u8::try_from(FieldElement::from(d)).ok())
-                        .map(|d| d as char)
-                        .collect();
-                    warn!("{} returned: {}", case_name, revert_message);
+                    let events = get_kakarot_execution_events(&call);
+                    if events.len() != 1 {
+                        warn!(
+                            "{} failed to find the single execution event: {:?}",
+                            case_name, events
+                        );
+                        return;
+                    }
+                    if events[0].data.0.last() == Some(&StarkFelt::ZERO) {
+                        let revert_message: String = call
+                            .execution
+                            .retdata
+                            .0
+                            .into_iter()
+                            .filter_map(|d| u8::try_from(FieldElement::from(d)).ok())
+                            .map(|d| d as char)
+                            .collect();
+                        warn!("{} returned: {}", case_name, revert_message);
+                    }
                 }
             }
         }
@@ -29,4 +43,21 @@ pub(crate) fn log_execution_result(
             error!("{} tx failed with:\n{:?}", case_name, err);
         }
     }
+}
+
+fn get_kakarot_execution_events(call_info: &CallInfo) -> Vec<EventContent> {
+    let mut events = Vec::new();
+    for c in call_info.into_iter() {
+        let mut filtered_events = c
+            .execution
+            .events
+            .iter()
+            .filter(|e| {
+                e.event.keys.get(0).map(|e| e.0) == Some(selector!("transaction_executed").into())
+            })
+            .map(|e| e.event.clone())
+            .collect();
+        events.append(&mut filtered_events);
+    }
+    events
 }
