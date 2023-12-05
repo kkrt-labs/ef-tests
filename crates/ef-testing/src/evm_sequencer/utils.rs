560e77d74cfb9559cde6f6383e495fc801554584
Elias Tazartes <elias.tazartes@gmail.com>
2023-12-05 16:09:14 -0300

    fix: fix signature logic

--- crates/ef-testing/src/evm_sequencer/utils.rs
+++ crates/ef-testing/src/evm_sequencer/utils.rs
@@ -115,8 +115,8 @@ pub fn to_broadcasted_starknet_transaction(
     let [r_low, r_high] = split_u256(signature.r);
     let [s_low, s_high] = split_u256(signature.s);
     let v = match transaction.transaction.tx_type() {
-        TxType::Legacy => signature.odd_y_parity as u64,
-        _ => signature.v(transaction.chain_id()),
+        TxType::Legacy => signature.v(transaction.chain_id()),
+        _ => signature.odd_y_parity as u64,
     };
     let signature = vec![
         FieldElement::from(r_low),
