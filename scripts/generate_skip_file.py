f2da179db01b96cee9d4d0ac83127cdf907b3e46
Clément Walter <clement0walter@gmail.com>
2024-01-17 16:35:38 +0100

    Use argparser to make the script a CLI tool

--- scripts/generate_skip_file.py
+++ scripts/generate_skip_file.py
@@ -1,41 +1,60 @@
+import argparse
 import re
 from collections import defaultdict
 
-with open("data.txt", "r") as f:
-    data = f.read()
 
-matches_failed = [
-    (
-        m.split("::")[-2]
-        .replace("_minus_", "-")
-        .replace("_plus_", "+")
-        .replace("_xor_", "^"),
-        m.split("::")[-1]
-        .replace("_minus_", "-")
-        .replace("_plus_", "+")
-        .replace("_xor_", "^"),
+def parse_and_write_to_yaml(input_file, output_file):
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    matches_failed = [
+        (
+            m.split("::")[-2]
+            .replace("_minus_", "-")
+            .replace("_plus_", "+")
+            .replace("_xor_", "^"),
+            m.split("::")[-1]
+            .replace("_minus_", "-")
+            .replace("_plus_", "+")
+            .replace("_xor_", "^"),
+        )
+        for m in re.findall(r"thread '(.*)' panicked at", data)
+    ]
+    summary = next(
+        re.finditer(
+            r"test result: (?P<result>\w+). (?P<passed>\d+) passed; (?P<failed>\d+) failed; (?P<ignored>\d+) ignored",
+            data,
+        )
     )
-    for m in re.findall(r"thread '(.*)' panicked at", data)
-]
-summary = next(
-    re.finditer(
-        r"test result: (?P<result>\w+). (?P<passed>\d+) passed; (?P<failed>\d+) failed; (?P<ignored>\d+) ignored",
-        data,
+
+    if len(matches_failed) != int(summary["failed"]):
+        raise ValueError("Failed to parse file")
+
+    skip_dict = defaultdict(list)
+    for [folder, file] in matches_failed:
+        skip_dict[folder].append(file)
+
+    skip = "testname:\n"
+    for folder in skip_dict:
+        skip += f"  {folder}:\n"
+        skip_dict[folder] = sorted(skip_dict[folder])
+        for file in skip_dict[folder]:
+            skip += f"      - {file[5:]}\n"
+
+    with open(output_file, "w") as f:
+        f.write(skip)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate skip file from ef tests logs"
     )
-)
+    parser.add_argument("input_file", help="Input file path")
+    parser.add_argument("output_file", help="Output file path")
 
-if len(matches_failed) != int(summary["failed"]):
-    raise ValueError("Failed to parse file")
+    args = parser.parse_args()
 
-skip_dict = defaultdict(list)
-for [folder, file] in matches_failed:
-    skip_dict[folder].append(file)
+    input_file = args.input_file
+    output_file = args.output_file
 
-skip = "filename:\n"
-for folder in skip_dict:
-    skip += f"  {folder}:\n"
-    for file in skip_dict[folder]:
-        skip += f"      - {file[5:]}\n"
-
-with open("blockchain-tests-skip.yml", "w") as f:
-    f.write(skip)
+    parse_and_write_to_yaml(input_file, output_file)
