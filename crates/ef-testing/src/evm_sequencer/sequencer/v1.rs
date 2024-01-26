20555c9d7ce5511d16fa73b98f15937e8f683195
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-26 10:29:10 +0100

    add the precompiles as a declared contract

--- crates/ef-testing/src/evm_sequencer/sequencer/v1.rs
+++ crates/ef-testing/src/evm_sequencer/sequencer/v1.rs
@@ -1,12 +1,7 @@
 use blockifier::{
-    abi::abi_utils::get_storage_var_address,
-    execution::contract_class::{ContractClass, ContractClassV0, ContractClassV1},
-    state::state_api::State as BlockifierState,
+    abi::abi_utils::get_storage_var_address, state::state_api::State as BlockifierState,
 };
-use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use cairo_vm::types::errors::program_errors::ProgramError;
 use lazy_static::lazy_static;
-use starknet::core::types::contract::{legacy::LegacyContractClass, CompiledClass};
 use starknet_api::hash::StarkFelt;
 
 use crate::evm_sequencer::{
@@ -19,7 +14,7 @@ use crate::evm_sequencer::{
         CHAIN_ID, ETH_FEE_TOKEN_ADDRESS, FEE_TOKEN_CLASS, FEE_TOKEN_CLASS_HASH, KAKAROT_ADDRESS,
         KAKAROT_OWNER_ADDRESS,
     },
-    types::contract_class::CasmContractClassWrapper,
+    sequencer::{convert_contract_class_v0, convert_contract_class_v1},
 };
 use sequencer::state::State as SequencerState;
 
@@ -45,39 +40,26 @@ lazy_static! {
         // Write the kakarot class and class hash.
         (&mut state).set_class_hash_at(*KAKAROT_ADDRESS, *KAKAROT_CLASS_HASH).expect("Failed to set kakarot class hash");
         (&mut state)
-            .set_contract_class(&KAKAROT_CLASS_HASH, convert_contract_class(&KAKAROT_CLASS).expect("Failed to convert KAKAROT CLASS to contract class")).expect("Failed to set kakarot contract class");
+            .set_contract_class(&KAKAROT_CLASS_HASH, convert_contract_class_v1(&KAKAROT_CLASS).expect("Failed to convert KAKAROT CLASS to contract class")).expect("Failed to set kakarot contract class");
 
         // Write eoa, contract account and uninitialized account.
         (&mut state).set_contract_class(
             &CONTRACT_ACCOUNT_CLASS_HASH,
-            convert_contract_class(&CONTRACT_ACCOUNT_CLASS).expect("Failed to convert CONTRACT ACCOUNT CLASS to contract class"),
+            convert_contract_class_v1(&CONTRACT_ACCOUNT_CLASS).expect("Failed to convert CONTRACT ACCOUNT CLASS to contract class"),
         ).expect("Failed to set contract account class");
         (&mut state)
-            .set_contract_class(&EOA_CLASS_HASH, convert_contract_class(&EOA_CLASS).expect("Failed to convert EOA CLASS to contract class")).expect("Failed to set eoa contract class");
+            .set_contract_class(&EOA_CLASS_HASH, convert_contract_class_v1(&EOA_CLASS).expect("Failed to convert EOA CLASS to contract class")).expect("Failed to set eoa contract class");
         (&mut state).set_contract_class(
             &UNINITIALIZED_ACCOUNT_CLASS_HASH,
-            convert_contract_class(&UNINITIALIZED_ACCOUNT_CLASS).expect("Failed to convert UNINITIALIZED ACCOUNT CLASS to contract class"),
+            convert_contract_class_v1(&UNINITIALIZED_ACCOUNT_CLASS).expect("Failed to convert UNINITIALIZED ACCOUNT CLASS to contract class"),
         ).expect("Failed to set uninitialized account class");
 
-        let convert_class = |class: &LegacyContractClass| -> Result<ContractClass, eyre::Error> {
-            Ok(ContractClass::V0(ContractClassV0::try_from_json_string(
-                &serde_json::to_string(&class).map_err(ProgramError::Parse)?,
-            )?))
-        };
         (&mut state).set_contract_class(
             &FEE_TOKEN_CLASS_HASH,
-            convert_class(&FEE_TOKEN_CLASS).expect("Failed to convert FEE TOKEN CLASS to contract class"),
+            convert_contract_class_v0(&FEE_TOKEN_CLASS).expect("Failed to convert FEE TOKEN CLASS to contract class"),
         ).expect("Failed to set fee token contract class");
         (&mut state).set_class_hash_at(*ETH_FEE_TOKEN_ADDRESS, *FEE_TOKEN_CLASS_HASH).expect("Failed to set fee token class hash");
 
         state
     };
-}
-
-fn convert_contract_class(class: &CompiledClass) -> Result<ContractClass, eyre::Error> {
-    let casm_contract_class = CasmContractClassWrapper::try_from(class)?;
-    let casm_contract_class: CasmContractClass = casm_contract_class.into();
-    Ok(ContractClass::V1(ContractClassV1::try_from(
-        casm_contract_class,
-    )?))
 }
