6ff2e84578af7f2bdf5d15f5e2933dbd929bbf2c
Mathieu <60658558+enitrat@users.noreply.github.com>
2024-02-08 11:07:04 +0100

    feat: skip nonce increment when eth validation failed in __execute__ (#653)
    * fit: skip nonce increment when eth validation failed in __execute__
    
    * fix check
    
    * Apply suggestions from code review
    
    Co-authored-by: greged93 <82421016+greged93@users.noreply.github.com>
    
    * address pr review
    
    * satisfy trunk check
    
    * make compilable
    
    ---------
    
    Co-authored-by: greged93 <82421016+greged93@users.noreply.github.com>

--- crates/ef-testing/src/models/result.rs
+++ crates/ef-testing/src/models/result.rs
@@ -11,19 +11,20 @@ use tracing::{error, info, warn};
 
 #[allow(clippy::cognitive_complexity)]
 pub(crate) fn log_execution_result(
-    result: TransactionExecutionResult<TransactionExecutionInfo>,
+    result: &TransactionExecutionResult<TransactionExecutionInfo>,
     case_name: &str,
     case_category: &str,
 ) {
     let case = format!("{}::{}", case_category, case_name);
     match result {
         TransactionExecutionResult::Ok(info) => {
-            if let Some(err) = info.revert_error {
+            /* trunk-ignore(clippy/option_if_let_else) */
+            if let Some(err) = info.revert_error.as_ref() {
                 warn!("{} reverted:\n{}", case, err.replace("\\n", "\n"));
             } else {
                 info!("{} passed: {:?}", case, info.actual_resources);
                 #[cfg(feature = "v0")]
-                if let Some(call) = info.execute_call_info {
+                if let Some(call) = info.execute_call_info.as_ref() {
                     use starknet::core::types::FieldElement;
                     use starknet_api::hash::StarkFelt;
                     let events = kakarot_execution_events(&call);
@@ -36,7 +37,7 @@ pub(crate) fn log_execution_result(
                         return;
                     }
                     if events[0].data.0.last() == Some(&StarkFelt::ZERO) {
-                        let return_data = call.execution.retdata.0;
+                        let return_data = call.execution.retdata.0.clone();
 
                         let revert_message_len = return_data.first().cloned().unwrap_or_default();
                         let revert_message_len =
@@ -72,7 +73,7 @@ pub(crate) fn log_execution_result(
             let re = regex::Regex::new(
                 r#"Error in the called contract \((0x[0-9a-zA-Z]+)\)[\s\S]*?EntryPointSelector\(StarkFelt\("(0x[0-9a-zA-Z]+)"\)\)"#,
             ).unwrap();
-            let matches: Vec<_> = re.captures_iter(&trace).map(|c| c.extract::<2>()).collect();
+            let matches: Vec<_> = re.captures_iter(trace).map(|c| c.extract::<2>()).collect();
             let last_match = matches.last().cloned().unwrap_or_default();
             warn!(
                 "Failed to find entrypoint {} for contract {}",
@@ -81,6 +82,27 @@ pub(crate) fn log_execution_result(
         }
         TransactionExecutionResult::Err(err) => error!("{} failed with:\n{:?}", case, err),
     }
+}
+
+pub(crate) fn extract_execution_retdata(
+    execution_info: TransactionExecutionInfo,
+) -> Option<String> {
+    if let Some(call) = execution_info.execute_call_info {
+        let call_exec = &call.execution;
+        let retdata = &call_exec.retdata;
+
+        // Skip the first byte which is the length of the return data
+        let retdata_bytes: Vec<u8> = retdata
+            .0
+            .iter()
+            .skip(1)
+            .filter_map(|felt| felt.bytes().last().cloned())
+            .collect();
+
+        let retdata_str: String = retdata_bytes.iter().map(|&c| c as char).collect();
+        return Some(retdata_str);
+    }
+    None
 }
 
 #[allow(dead_code)]
