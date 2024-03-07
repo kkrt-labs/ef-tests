4ce943d9bf3102f938a04fa5c08a440001b03eb3
enitrat <msaug@protonmail.com>
2024-03-07 17:28:28 +0100

    make clippy happy

--- crates/ef-testing/src/models/case.rs
+++ crates/ef-testing/src/models/case.rs
@@ -117,11 +117,9 @@ impl BlockchainTestCase {
         let sender_address = wallet.address().to_fixed_bytes();
 
         let maybe_revert_reason = String::from_utf8(output.return_data.as_slice().to_vec());
-        let eth_validation_failed = if let Ok(revert_reason) = maybe_revert_reason {
+        let eth_validation_failed = maybe_revert_reason.map_or(false, |revert_reason| {
             revert_reason == "Kakarot: eth validation failed"
-        } else {
-            false
-        };
+        });
 
         // Get gas_used and base_fee from RLP block - as in some cases, the block header is not present in the test data.
         let sealed_block = SealedBlock::decode(&mut self.block.rlp.as_ref())
