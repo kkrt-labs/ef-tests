992bc6426ed8b844a065c92dfb9a95b7054786c8
Gerson2102 <gersonloavas@gmail.com>
2024-04-25 16:10:35 -0600

    addressing enitrat comments

--- crates/ef-testing/src/evm_sequencer/evm_state/v0.rs
+++ crates/ef-testing/src/evm_sequencer/evm_state/v0.rs
@@ -92,6 +92,9 @@ impl Evm for KakarotSequencer {
         let starknet_address = self.compute_starknet_address(&evm_address)?;
 
         // Pick the class hash based on the account type.
+        //TODO: remove account distinction
+        if matches!(account.account_type, AccountType::EOA) {
+            self.state_mut().set_nonce(starknet_address, account.nonce);
 
         storage.append(&mut vec![
             starknet_storage!(ACCOUNT_IMPLEMENTATION, self.environment.eoa_class_hash.0), // both EOA and CA CH are the same (for now)
